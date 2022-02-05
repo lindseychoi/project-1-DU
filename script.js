ec5c0f65a2d325cdb072d79745b3f88549c19c10
wbgc728 <wbgc728@gmail.com>
2022-02-05 13:46:43 -0700

    Started pseudo coding

---
+++ script.js
@@ -0,0 +1,19 @@
+// Query variables
+var userLocation = document.querySelector('#input');
+
+
+// Location submit handler
+
+
+//Function to get Hiking Api
+
+
+//Funtion to get weather api
+
+
+//Print Trail Location rout
+
+//Print weather results in cards
+
+
+//
