746120439baf0ff938ea8e4b7ca2d54596338b2d
kkrogs <kourtney.krogman@gmail.com>
2022-02-07 21:29:04 -0700

    saving version one

--- script.js
+++ script.js
@@ -22,16 +22,33 @@ const openWeatherAPIKey = "61bd5a7935f37e9c18cacd14e8c89bc3";
 // 	console.error(err);
 // });
 
+//kkrogs
 //Need API to convert what is inputted and searched to LATITUDE and LONGITUDE; this is the Open Cage Geocoding API 
 // var requestOptions = {
 //     method: 'GET',
 //     redirect: 'follow'
 //   };
-  
-//   fetch("https://api.opencagedata.com/geocode/v1/json?q=Denver&key=0148c7965c584dfc849607c4be6c640b", requestOptions)
+//
+//Defining variables
+
+var apiGeoKey = "0148c7965c584dfc849607c4be6c640b";
+var srchInput = document.getElementbyID("#input-box").value;
+// my version of fetch
+fetch(`https://api.opencagedata.com/geocode/v1/json?q=${srchInput}&key=${apiGeoKey}`)
+    .then(response => response.text())
+   .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+
+//
+//   fetch(`https://api.opencagedata.com/geocode/v1/json?q=Denver&key=0148c7965c584dfc849607c4be6c640b", requestOptions)
+
 //     .then(response => response.text())
 //     .then(result => console.log(result))
 //     .catch(error => console.log('error', error));
+
+
+
 
 //5 day weather forecast from Open Weather, calls by city name ONLY//needs work...
 async function getFiveDayForecast(cityName) {
